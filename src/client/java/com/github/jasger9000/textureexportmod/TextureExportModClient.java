031c5c09135f7cbe0ef2105cbdb4957748dd3e63
J. Gerhards <g1.jasger@gmail.com>
2024-07-11 01:16:44 +0200

    init

---
+++ src/client/java/com/github/jasger9000/textureexportmod/TextureExportModClient.java
@@ -0,0 +1,9 @@
+package com.github.jasger9000.textureexportmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TextureExportModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
---
+++ src/main/java/com/github/jasger9000/textureexportmod/TextureExportMod.java
@@ -0,0 +1,14 @@
+package com.github.jasger9000.textureexportmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TextureExportMod implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("textureexportmod");
+
+	@Override
+	public void onInitialize() {
+	}
+}
